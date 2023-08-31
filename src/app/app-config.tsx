c58106071426b24076591743a64bd8b1592a5b1f
mike <95.michael.schroeder@gmail.com>
2023-08-30 20:47:56 -0500

    add tool model

--- src/app/app-config.tsx
+++ src/app/app-config.tsx
@@ -1,4 +1,4 @@
-import { Experience, PageSection, Project } from "./models"
+import { Experience, PageSection, Project, Tool } from "./models"
 import { ArrowUpperRight, hoverLinkCss, hoverShadowCss } from "./shared"
 import { easeInOutQuart } from "./shared"
 let pageKey = 0 // Do not touch
@@ -16,6 +16,33 @@ export const BIO: string[] = [
     'My focus these days is maintaining and developing new features for DS Smart, a mobile application for assisted living facilities.',
     'When I am not coding I am traveling with my wife, keeping up with friends online and dabbling in art/music.'
 ]
+
+export const PYTHON_TOOL: Tool = new Tool('python', 'https://www.python.org')
+export const AWS_TOOL: Tool = new Tool('aws', 'https://aws.amazon.com')
+export const SWIFT_TOOL: Tool = new Tool('swift', 'https://developer.apple.com/swift/')
+export const KOTLIN_TOOL: Tool = new Tool('kotlin', 'https://kotlinlang.org')
+export const ANDROID_TOOL: Tool = new Tool('android', 'https://developer.android.com')
+export const XCODE_TOOL: Tool = new Tool('xcode', 'https://developer.apple.com/xcode/')
+export const TERRAFORM_TOOL: Tool = new Tool('terraform', 'https://www.terraform.io')
+export const JAVA_TOOL: Tool = new Tool('java', 'https://www.java.com/en/')
+export const SPRING_BOOT_TOOL: Tool = new Tool('spring boot', 'https://spring.io/projects/spring-boot')
+export const ANGURLAR_TOOL: Tool = new Tool('angular', 'https://angular.io')
+export const JAVASCRIPT_TOOL: Tool = new Tool('javascript', 'https://angular.io')
+export const TYPESCRIPT_TOOL: Tool = new Tool('typescript', 'https://www.typescriptlang.org')
+export const PRIMEFLEX_TOOL: Tool = new Tool('primeflex', 'https://www.primefaces.org/primeflex/')
+export const SQL_TOOL: Tool = new Tool('sql', 'https://docs.oracle.com/en/database/oracle/oracle-database/19/sqlrf/#Oracle®-Database')
+export const NO_SQL_TOOL: Tool = new Tool('nosql', 'https://www.googleadservices.com/pagead/aclk?sa=L&ai=DChcSEwjk09OT2YWBAxU_bG8EHUtqA10YABACGgJqZg&ae=2&gclid=Cj0KCQjw0bunBhD9ARIsAAZl0E1ZzThFn1YIvUsIg-MFFrCR6lLNlmFT6wq92UA3yKssbjGx0QuyZwkaAhBtEALw_wcB&ohost=www.google.com&cid=CAESauD2vnn6Qtade-oOADBNGI1LjDpCSsMt22IubJK-vYv6ueGjH_i058hC51u_SvkeQKXsjrjSaW-ih1AwHQ6F6dwhuaz0s33mw9ItAzs8tvh5zgEDYg2s3SbtlGawHE_UXYGzCEITGFf3pd8&sig=AOD64_3Kk5CG7UBtg5NE-btjQiLKASDU4A&q&adurl&ved=2ahUKEwi8sMiT2YWBAxWDjYkEHQ8lB9oQ0Qx6BAgDEAE&nis=8&dct=1')
+export const JENKINS_TOOL: Tool = new Tool('jenkins', 'https://www.jenkins.io')
+export const AZURE_TOOL: Tool = new Tool('Azure', 'https://azure.microsoft.com/en-us/')
+export const TAILWIND_CSS_TOOL: Tool = new Tool('tailwind css', 'https://tailwindcss.com')
+export const REACT_TOOL: Tool = new Tool('react', 'https://react.dev')
+export const NEXTJS_TOOL: Tool = new Tool('next.js', 'https://nextjs.org')
+export const GODOT_4_TOOL: Tool = new Tool('godot 4', 'https://godotengine.org')
+export const BLENDER: Tool = new Tool('blender', 'https://www.blender.org')
+export const GIMP_TOOL: Tool = new Tool('gimp', 'https://www.gimp.org')
+export const GITLAB_CI_CD_TOOL: Tool = new Tool('gitlab ci/cd', 'https://docs.gitlab.com/ee/ci/')
+export const AMPLIFY_TOOL: Tool = new Tool('amplify', 'https://www.googleadservices.com/pagead/aclk?sa=L&ai=DChcSEwi4t43l4oWBAxX4bG8EHetCAmIYABAAGgJqZg&ae=2&gclid=Cj0KCQjw0bunBhD9ARIsAAZl0E0UNFH2WIAKpWsqqQ3xm3k4UAHqakOCVdLCCdAdQHXeEnut7LzXcysaArBrEALw_wcB&ohost=www.google.com&cid=CAESauD2_S8NvzxNUWwtnswtdky6rJSiPHyRPLT12QwCJJzsEv-BHGOnQr1NZ3hdhxQ8SkwexM-9Wo0Gq467Y_U_aybA2vAZTQt1nvgUA4vg4UgbbkxgkgEcL5O8jc2Y6YayOzH_OLtCXIcbji4&sig=AOD64_1BhHe_C36Qs29uw7YnhBJQBIl7pQ&q&adurl&ved=2ahUKEwia3IXl4oWBAxX5tIkEHUeUDEYQ0Qx6BAgIEAE&nis=8&dct=1')
+
 export const EXPERIENCE: Experience[] = [
     new Experience(
         'Software Engineer - Direct Supply',
@@ -24,13 +51,15 @@ export const EXPERIENCE: Experience[] = [
         'Present',
         'Support and develop new features for a multi-platform medical device application. We help our customers increase the efficiency of their workflows.',
         [
-            'Python',
-            'AWS',
-            'Swift',
-            'Kotlin',
-            'Android',
-            'IOS',
-            'Terraform'
+            PYTHON_TOOL,
+            AWS_TOOL,
+            SWIFT_TOOL,
+            KOTLIN_TOOL,
+            ANDROID_TOOL,
+            XCODE_TOOL,
+            TERRAFORM_TOOL,
+            NO_SQL_TOOL,
+            GITLAB_CI_CD_TOOL
         ]
     ),
     new Experience(
@@ -40,15 +69,15 @@ export const EXPERIENCE: Experience[] = [
         'January 2022',
         'Worked with various lines of business to develop Angular web applications and Java/Spring microservices. Our apps helped keep the business running efficiently.',
         [
-            'Java',
-            'Spring Boot',
-            'Angular',
-            'javascript',
-            'TypeScript',
-            'PrimeFlex',
-            'SQL',
-            'jenkins',
-            'Azure'
+            JAVA_TOOL,
+            SPRING_BOOT_TOOL,
+            ANGURLAR_TOOL,
+            JAVASCRIPT_TOOL,
+            TYPESCRIPT_TOOL,
+            PRIMEFLEX_TOOL,
+            SQL_TOOL,
+            JENKINS_TOOL,
+            AZURE_TOOL
         ]
     )
 ]
@@ -60,12 +89,12 @@ export const PROJECTS: Project[] = [
         'https://main.dg4tszwgq01x6.amplifyapp.com', 
         './images/project-1-peek.png',
         [
-            'AWS',
-            'Next.js',
-            'Amplify',
-            'Typescript',
-            'React',
-            'Tailwind CSS'
+            AWS_TOOL,
+            NEXTJS_TOOL,
+            AMPLIFY_TOOL,
+            TYPESCRIPT_TOOL,
+            REACT_TOOL,
+            TAILWIND_CSS_TOOL
         ], 
         'A responsive website for a local bar & grill. Designed to be incredibly flexible and reusable for other small businesses.'
     ),
@@ -74,10 +103,10 @@ export const PROJECTS: Project[] = [
         'https://youtu.be/uY1SxqblFFA',
         './images/project-2-peek.png',
         [
-            'Godot 4',
-            'Blender',
-            'Gimp',
-            'python'
+            GODOT_4_TOOL,
+            BLENDER,
+            GIMP_TOOL,
+            PYTHON_TOOL
         ],
         'This game is a product of a lot of practice over the years. I started in Unity, switched to Unreal Engine, and then finally found my favorite game engine Godot. It is an open source game engine that has seemless integration with Blender (a 3D modeling tool). I made all the assets, models and wrote all the code myself!'
     ),
@@ -86,12 +115,12 @@ export const PROJECTS: Project[] = [
         'https://main.diujez1kchjfo.amplifyapp.com', 
         './images/project-3-peek.png',
         [
-            'AWS',
-            'Next.js',
-            'Amplify',
-            'Typescript',
-            'React',
-            'Tailwind CSS'
+            AWS_TOOL,
+            NEXTJS_TOOL,
+            AMPLIFY_TOOL,
+            TYPESCRIPT_TOOL,
+            REACT_TOOL,
+            TAILWIND_CSS_TOOL
         ], 
         'A responsive website for a local bakery. My Next.js code is so flexible and reusable that!'
     ),
@@ -100,12 +129,12 @@ export const PROJECTS: Project[] = [
         'https://github.com/Mschroeder95/michael-schroeder-developer',
         './images/project-4-peek.png',
         [
-            'AWS',
-            'Next.js',
-            'Amplify',
-            'Typescript',
-            'React',
-            'Tailwind CSS'
+            AWS_TOOL,
+            NEXTJS_TOOL,
+            AMPLIFY_TOOL,
+            TYPESCRIPT_TOOL,
+            REACT_TOOL,
+            TAILWIND_CSS_TOOL
         ],
         'If you want a personal website feel free to fork mine and update the app-config.tsx to match your info! SO EASY!'
     )
@@ -212,10 +241,14 @@ export const PAGE_SECTIONS: PageSection[] = [
     })
 ]
 
-function createToolChip(tool: string): React.JSX.Element {
+function createToolChip(tool: Tool): React.JSX.Element {
     return (
-        <div className={`bg-secondary/30 rounded-3xl m-1 px-2 w-fit h-fit text-secondary hover:cursor-pointer ${hoverShadowCss}`}>
-            <p className="">{tool}</p>
+        <div className={`bg-secondary/30 rounded-3xl m-1 px-2 w-fit h-fit text-secondary hover:cursor-pointer ${hoverShadowCss}`} onClick={
+            () => {
+                window.location.href = tool.toolUrl
+            }
+        }>
+            <p className="">{tool.displayText}</p>
         </div>
     )
 }
